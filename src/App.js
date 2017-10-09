713e914fb45f8936fc837de84dbee31e43f83700
Alex Sinfarosa <as898@cornell.edu>
2017-10-09 14:56:54 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import Test from "components/Test";
+import { inject, observer } from "mobx-react";
+import StationsMenu from "components/StationsMenu";
 
+@inject("store")
+@observer
 class App extends Component {
   render() {
+    const { app } = this.props.store;
     return (
       <div>
-        <h1>Hello</h1>
-        <Test />
+        <StationsMenu store={app} />
       </div>
     );
   }
---
+++ src/components/StationsMenu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { stations } from "stations";
+
+const StationsMenu = ({ store }) => {
+  console.log(store.isLoading);
+  console.log(stations);
+  return <div />;
+};
+
+export default StationsMenu;
